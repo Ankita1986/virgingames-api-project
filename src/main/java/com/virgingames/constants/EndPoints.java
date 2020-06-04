f2268da6cb0200dd3c0408b1a8ced047ae5cd89a
Ankita1986 <ankitakaswala1@gmail.com>
2020-06-04 16:53:32 +0100

    virgingames-api-project

---
+++ src/main/java/com/virgingames/constants/EndPoints.java
@@ -0,0 +1,14 @@
+package com.virgingames.constants;
+
+/**
+ * Created by Ankita
+ */
+public class EndPoints {
+
+    /*
+     * Endpoints defined for the bingo lobby feed
+     */
+
+    public static final String GET_BINGO_LOBBY_FEED = "/GetBingoLobbyFeed.do";
+}
+
---
+++ src/main/java/com/virgingames/steps/GameSteps.java
@@ -0,0 +1,29 @@
+package com.virgingames.steps;
+
+import com.virgingames.constants.EndPoints;
+
+import io.restassured.parsing.Parser;
+import io.restassured.response.ValidatableResponse;
+import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.Step;
+
+
+/**
+ * Created by Ankita
+ */
+public class GameSteps {
+    /**
+     * Below programme to get virgin games information
+     */
+
+    @Step("Getting all games Information")
+    public ValidatableResponse gettingAllGamesInfo() {
+        return SerenityRest.rest().given()
+                .log().all()
+                .when()
+                .get(EndPoints.GET_BINGO_LOBBY_FEED)
+                .then().log().ifValidationFails()
+                .parser("text/plain;charset=UTF-8", Parser.JSON);
+
+    }
+}
